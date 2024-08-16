3405d0196c2010d1589bc28273c4dae1f2cf4b8e
s.nakamura <shotenba@gmail.com>
2024-08-17 01:10:16 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>あいうえお</h1>
+  <button
+    class="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700"
+  >
+    クリックしてね
+  </button>
+</template>
+
+<script setup lang="ts"></script>
